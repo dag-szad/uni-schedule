49e87e8803494cccc3d1719d5e2f000bcf829b6e
Dag <dagmara.szadkowska@wp.pl>
2024-07-18 18:10:19 +0200

    feat: update menu buttons to handle modal opening

--- src/components/Atoms/MenuButton/MenuButton.jsx
+++ src/components/Atoms/MenuButton/MenuButton.jsx
@@ -2,12 +2,30 @@ import termsButton from '../../../images/menu.svg';
 import profileButton from '../../../images/person.svg';
 
 const MenuButton = ({ selectedType }) => {
+  const leftModalOpen = () => {
+    const overlay = document.querySelector('#overlayLeft');
+    overlay.classList.toggle('hidden');
+    const modalLeft = document.querySelector('#modalLeft');
+    modalLeft.classList.toggle('hidden');
+  };
+
+  const rightModalOpen = () => {
+    const overlay = document.querySelector('#overlayRight');
+    overlay.classList.toggle('hidden');
+    const modalRight = document.querySelector('#modalRight');
+    modalRight.classList.toggle('hidden');
+  };
+
   return (
     <div>
       {selectedType === 'terms' ? (
-        <img src={termsButton} />
+        <button type="button" onClick={leftModalOpen}>
+          <img src={termsButton} />
+        </button>
       ) : (
-        <img src={profileButton} />
+        <button type="button" onClick={rightModalOpen}>
+          <img src={profileButton} />
+        </button>
       )}
     </div>
   );
