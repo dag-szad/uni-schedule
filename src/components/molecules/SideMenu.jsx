f7c73d96676596bc3d8e4df3dd1d90b065af1680
Dag <dagmara.szadkowska@wp.pl>
2024-08-06 16:45:24 +0200

    feat: update side menu display

--- src/components/molecules/SideMenu.jsx
+++ src/components/molecules/SideMenu.jsx
@@ -8,28 +8,31 @@ const SideMenu = ({
   onGroupChange,
   selectedGroup,
   saveHandler,
+  onOverlayClick,
 }) => {
+  const handleContainerClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div>
-      {menuType === 'left' ? (
-        <Overlay id="overlayLeft" isOpen={isOpen}>
-          <SideContainer id="modalLeft" $type={'left'}>
+    <Overlay isOpen={isOpen} onClick={onOverlayClick}>
+      <SideContainer onClick={handleContainerClick} $type={menuType}>
+        {menuType === 'left' ? (
+          <>
             <h1>SEMESTRY</h1>
             <p>Wybór semestrów: do zrobienia</p>
-          </SideContainer>
-        </Overlay>
-      ) : (
-        <Overlay id="overlayRight" isOpen={isOpen}>
-          <SideContainer id="modalRight" $type={'right'}>
+          </>
+        ) : (
+          <>
             <Groups
               onGroupChange={onGroupChange}
               selectedGroup={selectedGroup}
             />
             <SaveButton saveHandler={saveHandler} />
-          </SideContainer>
-        </Overlay>
-      )}
-    </div>
+          </>
+        )}
+      </SideContainer>
+    </Overlay>
   );
 };
 
